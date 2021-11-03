686dba31fb70d16d2fe4f6c169a98e39f0eb6039
1nwi <lllhn@naver.com>
2021-11-03 11:52:44 +0900

    [21.11.03] # ifm elif, while 학습

---
+++ day04/logical_quiz.py
@@ -0,0 +1,11 @@
+
+#정수 int  //  실수 float
+
+height = float(input('키: '))
+age = int(input('나이: '))
+
+if (height >= 140) and (age >= 8):
+    print('놀이기구에 탑승할 수 있습니다.')
+else:
+    print('놀이기구에 탑승할 수 없습니다.')
+print('오늘 하루 즐거운 시간되세요!')
---
+++ day04/while_quiz.py
@@ -0,0 +1,9 @@
+
+
+total = 0
+num_one = int(input('정수 1: '))
+num_two = int(input('정수 2: '))
+
+while num_one + num_two:
+    total += num_one + num_two
+print(f'{num_one}~{num_two}까지의 총합: {total}')
